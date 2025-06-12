f82609cae6f16e055337b6b8e445263d3512a3f5
codenester <codenester.dev@gmail.com>
2025-06-12 14:30:56 +0700

    remove to re-initialize

--- main-build.bat
+++
@@ -1,9 +0,0 @@
-rmdir .next /s /q
-rmdir dist /s /q
-mkdir dist
-copy "package.json" "dist/"
-copy "build.bat" "dist/"
-copy "server.js" "dist/"
-copy "web.config" "dist/"
-xcopy /i /s public dist\public
-npm run build:iis
--- src/components/search-list.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { forwardRef } from "react";
-
-const SearchList = forwardRef<any, { data: any[] }>((p, r) => {
-  return (
-    <div className="search-list"></div>
-  )
-});
-export default SearchList;
